e2af684a52baac82a1b1f40e86504dcf2f47c052
Pablo Gutierrez <pablo_gutierrez@35057.gnm.int>
2020-11-30 10:26:09 +0100

    years

--- atoms/fires/client/js/app.js
+++ atoms/fires/client/js/app.js
@@ -5,9 +5,8 @@ import countriesGeo from 'world-atlas/countries-50m.json'
 
 const countries = topojson.feature(countriesGeo, countriesGeo.objects.countries);
 
-const timeSpan = d3.scaleLinear()
-.domain([2000,2020])
-.range([0,19])
+const timeSpan = d3.utcYears(new Date(1999, 1, 1),new Date(2020, 1,  1),1)
+
 
 var width = 680,
     height = 500,
@@ -18,23 +17,23 @@ var projection = d3.geoEquirectangular()
 
 const path = d3.geoPath().projection(projection)
 
+
+const year = d3.select('.interactive-wrapper')
+.append('div')
+
 var svg = d3.select('.interactive-wrapper')
 .append('svg')
 .attr('id', 'map_container')
 .attr("viewBox", "0 0 " + width + " " + height )
 .attr("preserveAspectRatio", "xMinYMax");
 
-const appendImgs = (name) => {
 
-		svg.append("image")
-		.attr("clip-path", "url(#clip)")
-		.attr("xlink:href", `<%= path %>/jpeg/${name}.jpeg`)
-		.attr("width", width)
-		.attr("height", height - 10)
-		.attr('class', 'burned-img y' + name)
-}
-
-d3.utcYears(new Date(2000, 1, 1),new Date(2020, 1, 	1),1).map(d => appendImgs(d.getFullYear()))
+svg.append("image")
+.attr("clip-path", "url(#clip)")
+.attr("xlink:href", `<%= path %>/jpeg/hillshade.jpeg`)
+.attr("width", width)
+.attr("height", height - 12)
+.attr('class', 'hillshade')
 
 const map = svg
 .append('g')
@@ -47,7 +46,19 @@ const map = svg
 .attr('class', 'map-path')
 
 
-let images = svg.selectAll('image')
+const appendImgs = (name) => {
+
+		svg.append("image")
+		.attr("clip-path", "url(#clip)")
+		.attr("xlink:href", `<%= path %>/jpeg/${name}.jpeg`)
+		.attr("width", width)
+		.attr("height", height - 10)
+		.attr('class', 'burned-img y' + name)
+}
+
+timeSpan.map(d => appendImgs(d.getFullYear()))
+
+let images = svg.selectAll('.burned-img')
 
 let zero = 0;
 let selected = zero;
@@ -63,6 +74,8 @@ var t = d3.timer(d => {
 	})
 
   	d3.select(images.nodes()[selected]).classed('display', true)
+
+    year.html(timeSpan[selected].getFullYear())
 
   	selected++
 
